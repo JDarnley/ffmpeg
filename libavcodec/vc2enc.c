4a88a9000000760a97f137e993a858fd4bbd2a3e
James Darnley <jdarnley@obe.tv>
2017-08-16 20:11:13 +0200

    avcodec/vc2enc: write extended_transform_parameters for major version >= 3
    Unused for now but needed for fragments.

--- libavcodec/vc2enc.c
+++ libavcodec/vc2enc.c
@@ -545,6 +545,12 @@ static void encode_transform_params(VC2EncContext *s)
     put_vc2_ue_uint(&s->pb, s->wavelet_idx);
     put_vc2_ue_uint(&s->pb, s->wavelet_depth);
 
+    if (s->ver.major >= 3) {
+        /* extended_transform_parameters */
+        put_bits(&s->pb, 1, 0); /* asym_transform_index_flag */
+        put_bits(&s->pb, 1, 0); /* asym_transform_flag */
+    }
+
     encode_slice_params(s);
     encode_quant_matrix(s);
 }
