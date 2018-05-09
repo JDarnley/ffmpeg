48ce83dd7f986d882fb0796814ea8eb2608d1a80
James Darnley <jdarnley@obe.tv>
2018-05-09 15:16:33 +0200

    vc2enc: fix Legall 5,3 (5_3) wavelet transform

--- libavcodec/vc2enc_dwt.c
+++ libavcodec/vc2enc_dwt.c
@@ -217,7 +217,7 @@ static void legall_5_3_transform(dwtcoef *data,
 
     /* Vertical synthesis: Lifting stage 2. */
     data = data_original + stride*progress->vfilter_stage2;
-    line_max = line - 2;
+    line_max = y - 2;
     for (line = progress->vfilter_stage2; line < line_max; line += 2) {
         for (x = 0; x < synth_width; x++)
             data[x*hstride+stride] = LIFT2(data[x*hstride],
@@ -244,23 +244,18 @@ static void legall_5_3_transform(dwtcoef *data,
     progress->vfilter_stage2 = line;
 
     /* Vertical synthesis: Lifting stage 1. */
-    data = data_original + stride*progress->vfilter_stage1;
+    line_max = line;
     line = progress->vfilter_stage1;
-    if (line == 0 && progress->vfilter_stage2 > 0) {
+    data = data_original;
+    if (line == 0 && line_max > 0) {
         for (x = 0; x < synth_width; x++)
             data[x*hstride] = LIFT1(data[x*hstride + stride],
                                     data[x*hstride],
                                     data[x*hstride + stride]);
         line += 2;
     }
-    data += stride;
 
-    if (y != synth_height)
-        line_max -= 2;
-    /* Why subtract here?  It should be able to filter the same number of lines
-     * because it is filtering the one above.  I don't get it!  The block above
-     * doesn't need any of the lines processed here to remain unmodified, it is
-     * done with them. */
+    data += line*stride - stride;
     for (; line < line_max; line += 2) {
         for (x = 0; x < synth_width; x++)
             data[x*hstride + stride] = LIFT1(data[x*hstride],
