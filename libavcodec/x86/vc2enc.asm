4623e2b61026f421c79c030a1dc41428872bd60c
James Darnley <jdarnley@obe.tv>
2018-06-06 12:44:22 +0200

    turn legall_hfilter_stage1 into a macro and add avx version

--- libavcodec/x86/vc2enc.asm
+++ libavcodec/x86/vc2enc.asm
@@ -149,8 +149,42 @@ RET
 
 %endmacro
 
+%macro LEGALL_HFILTER_STAGE1 0
+
+cglobal legall_hfilter_stage1, 2, 2, 6, data_, w
+    mova m5, [pd_2]
+
+    ALIGN 16
+    .loop:
+        movu m0, [data_q]
+        movu m1, [data_q + mmsize]
+        movu m2, [data_q - 4]
+        movu m3, [data_q - 4 + mmsize]
+        shufps m4, m0, m1, q3131
+        shufps m0, m1, q2020
+        shufps m2, m3, q2020
+        SWAP 1,4
+
+        pslld m0, 1
+        paddd m2, m1
+        paddd m2, m5
+        psrad m2, 2
+        paddd m0, m2
+        SBUTTERFLY dq, 0, 1, 3
+
+        movu [data_q], m0
+        movu [data_q + mmsize], m1
+
+        add data_q, 2*mmsize
+        sub wd, mmsize/4
+    jg .loop
+RET
+
+%endmacro
+
 INIT_XMM sse2
 HAAR_BLOCK
+LEGALL_HFILTER_STAGE1
 
 cglobal legall_vfilter_stage1, 4, 6, 4, data_, stride_, w, h
     mov r4, data_q
@@ -210,35 +244,6 @@ cglobal legall_vfilter_stage2, 4, 6, 4, data_, stride_, w, h
     jg .loop_h
 RET
 
-cglobal legall_hfilter_stage1, 2, 2, 6, data_, w
-    mova m5, [pd_2]
-
-    ALIGN 16
-    .loop:
-        movu m0, [data_q]
-        movu m1, [data_q + mmsize]
-        movu m2, [data_q - 4]
-        movu m3, [data_q - 4 + mmsize]
-        shufps m4, m0, m1, q3131
-        shufps m0, m1, q2020
-        shufps m2, m3, q2020
-        SWAP 1,4
-
-        pslld m0, 1
-        paddd m2, m1
-        paddd m2, m5
-        psrad m2, 2
-        paddd m0, m2
-        SBUTTERFLY dq, 0, 1, 3
-
-        movu [data_q], m0
-        movu [data_q + mmsize], m1
-
-        add data_q, 2*mmsize
-        sub wd, mmsize/4
-    jg .loop
-RET
-
 cglobal legall_hfilter_stage2, 2, 2, 6, data_, w
     mova m5, [pd_1]
 
@@ -271,6 +276,7 @@ RET
 
 INIT_XMM avx
 HAAR_BLOCK
+LEGALL_HFILTER_STAGE1
 LOAD_PIXEL_DATA
 
 INIT_YMM avx2
