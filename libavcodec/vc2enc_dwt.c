59606dda1bec60e39de08e88ac6fe5575d517791
James Darnley <jdarnley@obe.tv>
2018-06-13 12:05:22 +0200

    vc2enc_dwt: support SIMD functions being NULL

--- libavcodec/vc2enc_dwt.c
+++ libavcodec/vc2enc_dwt.c
@@ -91,15 +91,18 @@ static void deslauriers_dubuc_9_7_transform(const VC2TransformContext *s,
     data = data_original + stride*progress->hfilter;
     for (line = progress->hfilter; line < y; line++) {
         /* Lifting stage 2. */
-        int temp = (width/2 - 2) & ~7;
+        int temp = 0;
 
         data[1] = LIFT2(data[0] << 1,
                         data[0] << 1,
                         data[1] << 1,
                         data[2] << 1,
                         data[4] << 1);
-        if (temp)
-            s->dd97_hfilter_stage2(data + 2, temp);
+        if (s->dd97_hfilter_stage2) {
+            temp = (width/2 - 2) & ~7;
+            if (temp)
+                s->dd97_hfilter_stage2(data + 2, temp);
+        }
         for (x = temp + 1; x < width/2 - 2; x++)
             data[2*x + 1] = LIFT2(data[2*x - 2] << 1,
                                   data[2*x    ] << 1,
@@ -118,10 +121,13 @@ static void deslauriers_dubuc_9_7_transform(const VC2TransformContext *s,
                               data[2*x + 2] << 1);
 
         /* Lifting stage 1. */
-        temp = (width/2 - 1) & ~7;
+        temp = 0;
         data[0] = LIFT1(data[1], data[0] << 1, data[1]);
-        if (temp)
-            s->legall_hfilter_stage1(data + 2, temp);
+        if (s->legall_hfilter_stage1) {
+            temp = (width/2 - 1) & ~7;
+            if (temp)
+                s->legall_hfilter_stage1(data + 2, temp);
+        }
         for (x = temp + 1; x < width/2; x++)
             data[2*x] = LIFT1(data[2*x-1],
                               data[2*x  ] << 1,
@@ -239,9 +245,13 @@ static void legall_5_3_transform(const VC2TransformContext *s, dwtcoef *data,
     data = data_original + stride*progress->hfilter;
     for (line = progress->hfilter; line < y; line++) {
         /* Lifting stage 2. */
-        int temp = (width/2 - 1) & ~7;
-        if (temp)
-            s->legall_hfilter_stage2(data, temp);
+        int temp = 0;
+
+        if (s->legall_hfilter_stage2) {
+            temp = (width/2 - 1) & ~7;
+            if (temp)
+                s->legall_hfilter_stage2(data, temp);
+        }
         for (x = temp; x < width/2 - 1; x++)
             data[2*x+1] = LIFT2(data[2*x  ] << 1,
                                 data[2*x+1] << 1,
@@ -252,8 +262,10 @@ static void legall_5_3_transform(const VC2TransformContext *s, dwtcoef *data,
 
         /* Lifting stage 1. */
         data[0] = LIFT1(data[1], data[0] << 1, data[1]);
-        if (temp)
-            s->legall_hfilter_stage1(data + 2, temp);
+        if (s->legall_hfilter_stage1) {
+            if (temp)
+                s->legall_hfilter_stage1(data + 2, temp);
+        }
         for (x = temp + 1; x < width/2; x++)
             data[2*x] = LIFT1(data[2*x-1],
                               data[2*x  ] << 1,
